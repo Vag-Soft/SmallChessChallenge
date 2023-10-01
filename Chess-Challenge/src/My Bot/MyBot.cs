4fb1552e8b238a7e7f85a5e27647a6be6fdae9c0
Evangelos Stylianos Softsis <esoftsis@csd.auth.gr>
2023-10-01 17:33:58 +0300

    Created a leaf evaluation function using only the material and the center squares on the chess boards

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -18,6 +18,73 @@ public class MyBot : IChessBot
     }
 
 
+    //Evaluation function that takes into account the pieces of each colour
+    public int BoardEvaluation(Board board, bool isWhite)
+    {
+        //Evaluating according to the board material
+        int whiteEval = board.GetPieceList(PieceType.Pawn, true).Count * 10 + (board.GetPieceList(PieceType.Knight, true).Count + board.GetPieceList(PieceType.Bishop, true).Count) * 30 + board.GetPieceList(PieceType.Rook, true).Count * 50 + board.GetPieceList(PieceType.Queen, true).Count * 90;
+        int blackEval = board.GetPieceList(PieceType.Pawn, false).Count * 10 + (board.GetPieceList(PieceType.Knight, false).Count + board.GetPieceList(PieceType.Bishop, false).Count) * 30 + board.GetPieceList(PieceType.Rook, false).Count * 50 + board.GetPieceList(PieceType.Queen, false).Count * 90;
+
+
+        //Evaluating according to center control
+        Piece midSquare = board.GetPiece(new Square("d4"));
+        if (!midSquare.IsNull)
+        {
+            if (midSquare.IsWhite)
+            {
+                whiteEval += 20;
+            }
+            else
+            {
+                blackEval += 20;
+            }
+        }
+        midSquare = board.GetPiece(new Square("d5"));
+        if (!midSquare.IsNull)
+        {
+            if (midSquare.IsWhite)
+            {
+                whiteEval += 20;
+            }
+            else
+            {
+                blackEval += 20;
+            }
+        }
+        midSquare = board.GetPiece(new Square("e4"));
+        if (!midSquare.IsNull)
+        {
+            if (midSquare.IsWhite)
+            {
+                whiteEval += 20;
+            }
+            else
+            {
+                blackEval += 20;
+            }
+        }
+        midSquare = board.GetPiece(new Square("e5"));
+        if (!midSquare.IsNull)
+        {
+            if (midSquare.IsWhite)
+            {
+                whiteEval += 20;
+            }
+            else
+            {
+                blackEval += 20;
+            }
+        }
+
+
+        if (isWhite)
+        {
+            return whiteEval - blackEval;
+        }
+        return blackEval - whiteEval;
+    }
+
+
     //Minimax search function with alpha beta pruning
     public int AlphaBetaSearch(Board board, ref Move bestMove, int depth, int initialDepth, int a, int b, bool isMaxPlayer, bool isWhite)
     {
